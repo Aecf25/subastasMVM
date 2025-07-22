92dbae50fa467d857323faa61d2205970e7e9456
unknown <adrianchanfreitas2@hotmail.com>
2025-07-22 05:18:15 -0400

    dddd

--- DBDjango/settings.py
+++ DBDjango/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
